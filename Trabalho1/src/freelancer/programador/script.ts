943795ab046c929451b4c9aba5e202692c25bb40
FelipeFlores77 <ruansilvaad012@gmail.com>
2026-04-17 20:34:48 -0300

    Adicionado os arquivos

--- Trabalho1/src/freelancer/programador/script.ts
+++ Trabalho1/src/freelancer/programador/script.ts
@@ -2,48 +2,66 @@ import { feedback } from "../../feedback/feedback"
 import { feedback1 } from "../../feedback/feedback"
 import { feedback2 } from "../../feedback/feedback"
 
-interface dev {
+export interface freelancer {
     nome: string
     listaServicos: string[]
-    nivel: 'J' | 'P' | 'S'
+    nivel: 'J' | 'P' | 'S' | 'Iniciante' | 'Intermediario' | 'Profissional'
     feedbacks: feedback[]
 }
 
-export let devs: dev[] = [
+export let devs: freelancer[] = [
     {
-        nome: 'teste',
-        listaServicos: ['1 - Fix Bugs', '2 - Landing Page','3 - Integrate API'],
+        nome: 'Caua Lima',
+        listaServicos: ['1 - Fix Bugs', '2 - Landing Page',],
         nivel: 'J',
-        feedbacks: [feedback1,feedback2]
+        feedbacks: []
     },
     {
-        nome: 'teste',
-        listaServicos: ['1 - Fix Bugs', '2 - Landing Page','3 - Integrate API'],
+        nome: 'Carlos Eduardo',
+        listaServicos: ['1 - Landing Page','2 - Integrate API'],
         nivel: 'J',
-        feedbacks: [feedback1,feedback2]
+        feedbacks: []
     },
     {
-        nome: 'teste',
-        listaServicos: ['1 - Fix Bugs', '2 - Landing Page','3 - Integrate API'],
+        nome: 'Miguel Gustavo',
+        listaServicos: ['1 - Fix Bugs','2 - Integrate API'],
         nivel: 'J',
-        feedbacks: [feedback1,feedback2]
+        feedbacks: []
     },
     {
-        nome: 'teste',
-        listaServicos: ['1 - Fix Bugs', '2 - Landing Page','3 - Integrate API'],
-        nivel: 'J',
-        feedbacks: [feedback1,feedback2]
+        nome: 'Joao Silva',
+        listaServicos: ['1 - Fix Bugs','2 - Integrate API'],
+        nivel: 'S',
+        feedbacks: []
     },
     {
-        nome: 'teste',
+        nome: 'Felipe Flores',
         listaServicos: ['1 - Fix Bugs', '2 - Landing Page','3 - Integrate API'],
-        nivel: 'J',
-        feedbacks: [feedback1,feedback2]
+        nivel: 'S',
+        feedbacks: []
     },
     {
-        nome: 'teste',
-        listaServicos: ['1 - Fix Bugs', '2 - Landing Page','3 - Integrate API'],
-        nivel: 'J',
-        feedbacks: [feedback1,feedback2]
+        nome: 'Bruno Krause',
+        listaServicos: ['1 - Fix Bugs','2 - Landing Page','3 - Integrate API'],
+        nivel: 'S',
+        feedbacks: []
+    },
+    {
+        nome: 'Eduardo Alfonso',
+        listaServicos: ['1 - Fix Bugs','2 - Integrate API'],
+        nivel: 'P',
+        feedbacks: []
+    },
+    {
+        nome: 'Pedro WIlges',
+        listaServicos: ['1 - Fix Bugs', '2 - Landing Page'],
+        nivel: 'P',
+        feedbacks: []
+    },
+    {
+        nome: 'Joao Fiuza',
+        listaServicos: ['1 - Fix Bugs'],
+        nivel: 'P',
+        feedbacks: []
     }
 ]

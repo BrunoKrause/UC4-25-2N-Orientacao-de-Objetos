73e56550699c7742ccc39b3808cda43e454c7991
BRUNO KRAUSE NOIA DA COSTA <02155268050@senacrs.edu.br>
2026-04-17 20:35:47 -0300

    Sei lá o que fiz

--- Trabalho1/src/adicionais/model.ts
+++ Trabalho1/src/adicionais/model.ts
@@ -1,4 +1,42 @@
-export interface adicional {
-    getDescricao(): string
-    getPreco(): number
+import rl from 'readline-sync'
+import { fixBugs } from '../servicos/programador/fixBugs'
+
+const fixBugs1 = new fixBugs(800)
+
+export const adicionais = [
+    {
+        id: 1,
+        nome: 'Urgência',
+        tipo: 'Porcentagem',
+        value: 50,
+        multiplo: false
+    },
+    {
+        id: 2,
+        nome: 'Prioridade',
+        tipo: 'Porcentagem',
+        value: 30,
+        multiplo: false    
+    },
+    {
+        id: 3,
+        nome: 'Bônus',
+        tipo: 'Fixo',
+        value: 200,
+        multiplo: true
+    }
+]
+
+console.table(adicionais, ["id", "nome"]);
+
+let i = rl.questionInt('Escolha um bônus:') - 1
+
+switch(i) {
+    case 1:
+        fixBugs1.preco += fixBugs1.preco * (50 / 100)
+        console.log(fixBugs1.getPreco())
+        
+        break
+    default:
+        break
 }
--- Trabalho1/src/freelancer/programador/script.ts
+++ Trabalho1/src/freelancer/programador/script.ts
@@ -2,14 +2,14 @@ import { feedback } from "../../feedback/feedback"
 import { feedback1 } from "../../feedback/feedback"
 import { feedback2 } from "../../feedback/feedback"
 
-interface dev {
+export interface freelancer {
     nome: string
     listaServicos: string[]
     nivel: 'J' | 'P' | 'S'
     feedbacks: feedback[]
 }
 
-export let devs: dev[] = [
+export let devs: freelancer[] = [
     {
         nome: 'teste',
         listaServicos: ['1 - Fix Bugs', '2 - Landing Page','3 - Integrate API'],
